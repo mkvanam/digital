18c29972fbdbf5b32a484fd54c4413c2d52906c7
mkvanam <mkvanam@gmail.com>
2023-05-21 09:44:47 +0530

    version2

--- GIT_Digital_Pjt/src/SampleClass.java
+++ GIT_Digital_Pjt/src/SampleClass.java
@@ -4,6 +4,8 @@ public class SampleClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git commit from Eclipse");
+		System.out.println("Git commit from Eclipse1");
+		System.out.println("Git commit from Eclipse2");
 
 	}
 
